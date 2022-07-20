bcde9379ae5a6b539e41aaa3fb79bda2cff75fb8
Patrick <patrickonesradaelli@gmail.com>
2022-07-20 20:39:44 -0300

    comentario

--- app/Models/Clientes.php
+++ app/Models/Clientes.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class Clientes extends Model
+class User extends Model implements Auditable
 {
+    use \OwenIt\Auditing\Auditable;
+
     protected $table = 'cliente';
     
 
